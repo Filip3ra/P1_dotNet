3d7e686707bcdb9e19bf3ebe0bb7686d11fee097
Filip3ra <filipimacielrj@gmail.com>
2024-03-28 10:14:51 -0300

    estudo para prova

---
+++ q1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+/* Questão 1 a)
+
+int i = 1;
+switch (i)
+{
+  case 1:
+    break;
+  case 2: Console.WriteLine("Um"); break;
+  default:
+    Console.WriteLine("Outro");
+    break;
+}
+*/
+
+/* Questão 1 b) 
+
+int x = 2, y = 3;
+Console.WriteLine((double)x / y);
+*/
+
+/* Questão 1 c)
+double x, y;
+x = y = 0.0;
+
+Console.WriteLine(x / y);
+ */
+
+/* Questão 1 d)*/
+
+
